8900f54536f8152e693d58ce1ef0adbeb09bb167
정연빈 <yeonbn88@gmail.com>
2019-12-16 02:52:03 +0900

    order domain business modeling

--- src/main/java/jpabook/jpashop/domain/item/Order.java
+++ src/main/java/jpabook/jpashop/domain/item/Order.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.status.DeleveryStatus;
 import jpabook.jpashop.status.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,10 +8,12 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static javax.persistence.CascadeType.ALL;
-import static javax.persistence.FetchType.*;
+import static javax.persistence.FetchType.LAZY;
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -50,5 +53,40 @@ public class Order {
     public void setDelivery(Delivery delivery){
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    // 생성 메소드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        Arrays.stream(orderItems)
+                .forEach(orderItem -> order.addOrderItem(orderItem));
+
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    // 비즈니스 로직
+    // 주문 취소
+    public void cancel() {
+        if(delivery.getStatus() == DeleveryStatus.COMPLETE) {
+            throw new IllegalStateException("이미 배송이 완료된 상품은 취소를 할 수 없습니다.");
+        }
+
+        this.setOrderStatus(OrderStatus.CANCEL);
+        this.getOrderItemList().stream()
+                .forEach(orderItem -> orderItem.cancel());
+    }
+
+    // 조회 로직
+    // 주문 금액조회
+    public int getTotalPrice() {
+        return orderItemList.stream()
+                .mapToInt(orderItem -> orderItem.getTotalPrice())
+                .sum();
+
     }
 }
---
+++ src/test/java/jpabook/jpashop/domain/item/OrderItemTest.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.domain.item;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class OrderItemTest {
+
+    @Test
+    public void streamTest() {
+        // given
+        OrderItem orderItem1 = new OrderItem();
+        orderItem1.setCount(2);
+        orderItem1.setOrderPrice(10);
+
+        OrderItem orderItem2 = new OrderItem();
+        orderItem2.setCount(2);
+        orderItem2.setOrderPrice(20);
+
+        Order order = new Order();
+        order.addOrderItem(orderItem1);
+        order.addOrderItem(orderItem2);
+
+        // when
+        int totalPrice = order.getTotalPrice();
+
+        // then
+        assertEquals(totalPrice, 60);
+    }
+}
